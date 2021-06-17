e65c3f22fb65ceb3f05b14b488af9c1abc15b671
SaltwaterC <saltwaterc@gmail.com>
2021-06-17 22:49:30 +0100

    Update Elrond test target. Refactor versions management.

--- .kitchen_common.rb
+++ .kitchen_common.rb
@@ -1,15 +1,8 @@
-def cinc_version
-  '16.11.7'
-end
-
-def elrond_version
-  '1.1.63.0'
-end
-
-def ubuntu_version
-  '20.04'
-end
-
-def oracle_version
-  '8.4'
+def versions
+  {
+    elrond: '1.1.64.0',
+    cinc: '16.11.7',
+    ubuntu: '20.04',
+    oracle: '8.4',
+  }
 end
