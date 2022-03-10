19a499f43fa9aa2dd79c8f607f5f08813454a4b1
SaltwaterC <saltwaterc@gmail.com>
2022-03-10 01:28:09 +0000

    Bump test version to 1.3.8.0.

--- .kitchen_common.rb
+++ .kitchen_common.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 def versions
+  elrond_test = '1.3.8.0'
+
   {
-    elrond: ENV['ELROND_VERSION'] || '1.2.38.2-rc2',
+    elrond: ENV['ELROND_VERSION'] || elrond_test,
     cinc: '17.9.26',
     ubuntu: '20.04',
     oracle: '8.5'
