86a9d847050437bd2440364a521b7145c01c08bd
SaltwaterC <saltwaterc@gmail.com>
2022-07-08 02:07:41 +0100

    Bump test version to 1.34. Bump oracle to 8.6.

--- .kitchen_common.rb
+++ .kitchen_common.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 def versions
-  elrond_test = '1.3.16.0'
+  elrond_test = '1.3.34.0'
 
   {
     elrond: ENV['ELROND_VERSION'] || elrond_test,
     cinc: '17.9.26',
     ubuntu: '20.04',
-    oracle: '8.5'
+    oracle: '8.6'
   }
 end
